7eff4b061c6901733f7c397b2efd18e80af49060
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2022-07-10 11:43:49 +0300

    created error handler

--- back/error/errorHandler.ts
+++ back/error/errorHandler.ts
@@ -1,1 +1,36 @@
+import { NextFunction, Request, Response } from 'express';
+
 // Todo create and export errorHandler
+import log from '../utils/log';
+
+const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { type, error, msg } = err;
+  switch (type) {
+    case 404:
+      log(msg);
+      log(error);
+      res
+        .status(404)
+        .send(
+          'Could not get a response from the DB. Either a problem with the query or the DB server. Check server logs for details.'
+        );
+      break;
+    case 503:
+      log('Failed connecting to DB');
+      res.status(503).send('Failed connecting to DB');
+      break;
+
+    default:
+      log('OH NO! something went wrong! check logs for details');
+      log(error);
+      res.status(400).send('something went wrong! check logs for details');
+      break;
+  }
+};
+
+module.exports = { errorHandler };
