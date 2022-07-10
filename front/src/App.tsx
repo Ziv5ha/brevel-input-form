358b3494b7af2f32d0b7a7d1c704871b98e6421c
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2022-07-10 13:02:57 +0300

    init react front

---
+++ front/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// ts-lint ignore
+import logo from './logo.jpg';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <img src={logo} className='App-logo' alt='logo' />
+    </div>
+  );
+}
+
+export default App;
