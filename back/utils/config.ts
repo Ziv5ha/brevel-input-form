3f7fff60d58baf0c802c756c95251838457083ec
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2022-07-10 12:03:44 +0300

    add postgreSQL docker for development

--- back/utils/config.ts
+++ back/utils/config.ts
@@ -1,10 +1,14 @@
-require('dotenv').config();
+// require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
+
 const config = {
   HOST: process.env.HOST || 'localhost',
   PORT: parseInt(process.env.PORT || '5432'),
   USER: process.env.USER || 'username',
   PASSWORD: process.env.PASSWORD || 'password',
-  DATABASE: process.env.DATABASE || 'database',
+  DATABASE: process.env.DATABASE || '',
   CONNECTION_STRING: process.env.CONNECTION_STRING,
 };
+
 export default config;
